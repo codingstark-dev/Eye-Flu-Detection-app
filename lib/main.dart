12d527b6124cdbd4b3456f9759580559fd212aa1
“Himanshu” <mhimanshu.338@gmail.com>
2024-03-19 20:11:04 +0530

    Added SplashScreen import and update home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eyeflu/SplashScreen.dart';
 import 'package:eyeflu/ts.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TfliteModel(),
+      home:  SplashScreen(),
     );
   }
 }
