f0edfa4123a13c996720871e534a8078b0ada01d
“Himanshu” <mhimanshu.338@gmail.com>
2024-03-19 20:11:25 +0530

    Refactored TfliteModel class and add image selection options

--- lib/ts.dart
+++ lib/ts.dart
@@ -13,26 +13,24 @@ class TfliteModel extends StatefulWidget {
 }
 
 class _TfliteModelState extends State<TfliteModel> {
-  
   late File _image;
   late List _results;
-  bool imageSelect=false;
+  bool imageSelect = false;
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
     loadModel();
   }
-  Future loadModel()
-  async {
+
+  Future loadModel() async {
     Tflite.close();
     String res;
-    res=(await Tflite.loadModel(model: "assets/model.tflite",labels: "assets/labels.txt"))!;
+    res = (await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt"))!;
     print("Models loading status: $res");
   }
 
-  Future imageClassification(File image)
-  async {
+  Future imageClassification(File image) async {
     final List? recognitions = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 6,
@@ -41,49 +39,73 @@ class _TfliteModelState extends State<TfliteModel> {
       imageStd: 127.5,
     );
     setState(() {
-      _results=recognitions!;
-      _image=image;
-      imageSelect=true;
+      _results = recognitions!;
+      _image = image;
+      imageSelect = true;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
         title: const Text("Eye Flu Detection"),
       ),
-      body: ListView(
-        children: [
-          (imageSelect)?Container(
-        margin: const EdgeInsets.all(10),
-        child: Image.file(_image),
-      ):Container(
-        margin: const EdgeInsets.all(10),
-            child: const Opacity(
-              opacity: 0.8,
-              child: Center(
-                child: Text("No image selected"),
-              ),
-            ),
-      ),
-          SingleChildScrollView(
-            child: Column(
-              children: (imageSelect)?_results.map((result) {
-                return Card(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                      "${result['label']} - ${result['confidence'].toStringAsFixed(2)}",
-                      style: const TextStyle(color: Colors.red,
-                      fontSize: 20),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            (imageSelect)
+                ? Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Image.file(_image),
+                  )
+                : Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: const Opacity(
+                            opacity: 0.8,
+                            child: Center(
+                              child: Text(
+                                "No image selected",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                );
-              }).toList():[],
-
-            ),
-          )
-        ],
+            SingleChildScrollView(
+              child: Column(
+                children: (imageSelect)
+                    ? _results.map((result) {
+                        return Card(
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            child: Text(
+                              "${result['label']} -  Rate:  ${(result['confidence'] * 100).toStringAsFixed(0)}%",
+                              style: const TextStyle(
+                                  color: Colors.red, fontSize: 20),
+                            ),
+                          ),
+                        );
+                      }).toList()
+                    : [],
+              ),
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: pickImage,
@@ -92,13 +114,41 @@ class _TfliteModelState extends State<TfliteModel> {
       ),
     );
   }
-  Future pickImage()
-  async {
+
+  Future pickImage() async {
     final ImagePicker _picker = ImagePicker();
-    final XFile? pickedFile = await _picker.pickImage(
-      source: ImageSource.gallery,
-    );
-    File image=File(pickedFile!.path);
-    imageClassification(image);
+    final XFile? pickedFile = await showDialog<XFile>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Choose option'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: const Text('Take a picture of eye'),
+                    onTap: () async {
+                      Navigator.pop(context,
+                          await _picker.pickImage(source: ImageSource.camera));
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0)),
+                  GestureDetector(
+                    child: const Text('Select from gallery'),
+                    onTap: () async {
+                      Navigator.pop(context,
+                          await _picker.pickImage(source: ImageSource.gallery));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+
+    if (pickedFile != null) {
+      File image = File(pickedFile.path);
+      imageClassification(image);
+    }
   }
 }
